8806bdb7b120ebd5c982b71ef3901d38497847f8
laleoarrow <luao@stu.cqmu.edu.cn>
2025-05-18 16:18:30 +0800

    Initial commit

---
+++ R/1.R
@@ -0,0 +1,54 @@
+#' Give Messages with my color
+#'
+#' @param ... The messages you wanna messgae, which will be pasted together
+#' @param color Str. Preferred color. Default is yellow.
+#'              Options are "31" (red), "32" (green), "34" (blue), "95" (light purple)...
+#' @param return Logical. If TRUE, returns the formatted string. If FALSE (Default), prints directly.
+#'
+#' @export
+#' @examples
+#' leo_message("This is a red message", color = "31")
+#' leo_message("This is a green message", "\nhaha", color = "32")
+#' leo_message("This is a blue ", "message", color = "34")
+#' leo_message("This is a light purple message", color = "95")
+#' leo_message(" 🦁🦁🦁 Welcome to use the LEO package ! 🦁🦁🦁")
+leo_message <- function(..., color = "31", return = FALSE) {
+  message_content <- paste0(..., collapse = " ")
+  formatted_message <- paste0("\033[", color, "m", message_content, "\033[0m")
+  message(formatted_message)
+}
+
+#' Log Messages with Timestamps
+#'
+#' Logs messages with timestamps
+#' The messages are styled using the `cli` package for enhanced readability.
+#' This function can not deal with {} function in the `cli` package.
+#'
+#' @param ... The message string to log, which will be pasted together.
+#' @param level The log level. Options are `"info"`, `"success"`, `"warning"`, and `"danger"`.
+#' @param levels All levels that is now supported.
+#'
+#' @return No return value. Outputs a formatted log message with a timestamp.
+#' @export
+#'
+#' @examples
+#' n1 <- 10; n2 <- 20
+#' leo_log("Processing the", n1, "and", n2, "files.")
+#' leo_log("Task completed successfully!", level = "success")
+#' leo_log("Potential issue detected.", level = "warning")
+#' leo_log("Error occurred during processing!", level = "danger")
+leo_log <- function(..., level = "info") {
+  msg <- paste(..., collapse = " "); timestamp <- paste0("[", format(Sys.time(), '%H:%M'),  "]")
+  timestamp_colored <- switch(level,
+                              "info" = cli::col_cyan(timestamp),     # cyan
+                              "success" = cli::col_green(timestamp), # green
+                              "warning" = cli::col_yellow(timestamp),# yellow
+                              "danger" = cli::col_red(timestamp))    # red
+  formatted_message <- paste(timestamp_colored, msg)
+  switch(level,
+         "info"    = cli::cli_alert_info(formatted_message),
+         "success" = cli::cli_alert_success(formatted_message),
+         "warning" = cli::cli_alert_warning(formatted_message),
+         "danger"  = cli::cli_alert_danger(formatted_message)
+  )
+}
